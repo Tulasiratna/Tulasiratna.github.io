087bc1672162209a809f94820b04a25790aea3c5
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-01-02 22:57:51 +0100

    Inserted html into js

--- code/Tulasiratna/my-app/src/index.js
+++ code/Tulasiratna/my-app/src/index.js
@@ -1,2 +1,14 @@
 import React from 'react'; // library name react
 import ReactDOM from 'react-dom'; // library name react-dom
+
+const App = () => {
+  return(
+    <div>My First React App!!!!!!!</div>
+  )
+}
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+
+)
