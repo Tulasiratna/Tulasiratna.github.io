9e2acf6a27a4b81cce6a78f7263aa4a25a81f4b7
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-01-07 15:49:18 +0100

    JSX with javascript variables

--- code/Tulasiratna/my-app/src/index.js
+++ code/Tulasiratna/my-app/src/index.js
@@ -2,11 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
+  const buttonText = { text: 'Reach me!!!' }
   return (
     <div>
-      <label for="name" className="label">Enter e-mail</label>
+      <label htmlFor="name" className="label">Enter e-mail</label>
       <input id="name" type="text"></input>
-      <button style={{backgroundColor:'red', color:'white'}}>Submit</button>
+
+      <button style={{backgroundColor:'red', color:'white'}}>{ buttonText.text }</button>
+
     </div>
   )
 }
