b355eeb8f20ad8c1870cf2ed62b784c98f6cf3d9
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-01-07 20:56:23 +0100

    new react app created and styled with semantic ui

---
+++ code/Tulasiratna/blog-posts/src/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+const App = () => {
+  return(
+      <div>
+        Hello World!!!!!!!!!!!
+      </div>
+    )
+}
+
+ReactDOM.render(
+  <App />,
+  document.getElementById('root')
+)
