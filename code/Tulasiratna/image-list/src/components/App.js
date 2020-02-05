c08991f1d39320cd35c38ebf7ee3ee548401266d
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-02-05 12:01:08 +0100

    handling with forms in react

---
+++ code/Tulasiratna/image-list/src/components/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Searchinput from './Searchinput';
+
+const App = () => {
+  return (
+    <div>
+      <form action="">
+        <input type="text" />
+      </form>
+    </div>
+  )
+}
+
+export default App;
