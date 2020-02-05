508722af7b75c95b40932fb69e50661502380b7d
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-02-05 12:50:42 +0100

    event handler method

--- code/Tulasiratna/image-list/src/components/Searchinput.js
+++ code/Tulasiratna/image-list/src/components/Searchinput.js
@@ -1,13 +1,21 @@
 import React from 'react';
 
 class Searchinput extends React.Component {
+  onInputChange(event) {
+    console.log(event.target.value);
+
+  }
   render() {
     return(
       <div className="ui segment">
         <form className="ui form">
           <div className="filed">
             <div className="ui massive icon input">
-              <input type="text" placeholder="search..." />
+              <input
+                type="text"
+                placeholder="search..."
+                onChange={this.onInputChange}
+              />
               <i className="search icon"></i>
             </div>
           </div>
