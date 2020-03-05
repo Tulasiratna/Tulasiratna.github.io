7684e9430c976d6e91d3fb3f98ae54309afef684
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-03-05 14:21:13 +0100

    create react app from scratch and list items

---
+++ code/Tulasiratna/app/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(
+  <div>
+    <h1>My Favourite Foods</h1>
+    <ul>
+      <li>Chiicken</li>
+      <li>Fish</li>
+      <li>Chocolate Mouse</li>
+    </ul>
+  </div>,
+  document.getElementById('root')
+);
+
