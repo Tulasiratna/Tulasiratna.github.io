8c414d40c2bebfd3e5b1a358f740670a2fdfbfb9
Tulasi ratna <ratna.tulasi341@gmail.com>
2020-03-09 11:07:08 +0100

    react components practice

---
+++ code/Tulasiratna/app/src/List.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function List() {
+  return  <ul>
+            <li>Chicken</li>
+            <li>Noodiles</li>
+            <li>Chocolate Mousse</li>
+          </ul>
+}
+
+export default List;
--- code/Tulasiratna/app/src/index.js
+++ code/Tulasiratna/app/src/index.js
@@ -1,15 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import App from './App';
+
 
 ReactDOM.render(
-  <div>
-    <h1>My Favourite Foods</h1>
-    <ul>
-      <li>Chiicken</li>
-      <li>Fish</li>
-      <li>Chocolate Mouse</li>
-    </ul>
-  </div>,
+
+    <App />,
+
   document.getElementById('root')
 );
+
 
